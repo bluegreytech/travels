f527dab64c749c283b986595ce1ee7059fa5c328
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-12-19 05:34:37 -0500

    add

---
+++ php pages/cab-search.php
@@ -0,0 +1,103 @@
+<?php include 'header.php';?>
+
+  	<!--Page Cover-->
+  	<section class="row page-cover">
+  		<div class="container">
+  			<h2 class="h1 page-title">Booking Confirmation</h2>
+  			<ol class="breadcrumb">
+  				<li><a href="index.php">Home</a></li>
+  				<li class="active">Booking Confirmation</li>
+  			</ol>
+  		</div>
+  	</section> 	
+  	<section class="wrapper-about">
+		<div class="container">
+			<div class="media fleet fleet-list">
+				<div class="row">
+					<div class="col-md-5  col-sm-12 col-xs-12">
+						<div class="media-left"><img src="images/sedan-hover.png" alt=""></div>
+						<div class="row specification eminities">
+							<ul class="nav">
+								<li><span><img src="images\icons\fleet-list\1.png" alt=""></span>Auto trasition</li>
+								<li><span><img src="images\icons\fleet-list\2.png" alt=""></span>06 list/100 kms</li>
+								<li><span><img src="images\icons\fleet-list\3.png" alt=""></span>02 seats</li>
+								<li><span><img src="images\icons\fleet-list\4.png" alt=""></span>Make Year 2016</li>
+								<li><span><img src="images\icons\fleet-list\5.png" alt=""></span>04 air bags</li>
+								<li><span><img src="images\icons\fleet-list\6.png" alt=""></span>100 lits capity</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-7 col-sm-12 col-xs-12">
+						<div class="media-body this-body">
+							<div class="media this-header">
+								<div class="media-body">
+									<h2 class="rent">Sedan</h2>						
+								</div>
+							</div>
+							<div class="summary row m0">
+								Rental car companies are notorious for finding additional surcharges, sometimes not added until the point of sale. These can include location or airport taxe...
+							</div>
+							<form action="login.php">
+								<div class="row specification car-type">
+								    <label for="Indica Vista">Indica Vista</label>
+									<label for="Suzuki Swift">Suzuki Swift</label>
+									<label for="Hyundai Eon">Hyundai Eon</label>
+									<label for="Toyota Liva">Toyota Liva</label>
+									<label for="Dustan Go">Dustan Go</label>
+								</div>
+								<div class="row specification">
+									<input type="submit" value="Book a cab" class="btn btn-primary">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>				
+			</div>
+
+			<div class="media fleet fleet-list">
+				<div class="row">
+					<div class="col-md-5  col-sm-12 col-xs-12">
+						<div class="media-left"><img src="images/suv-hover.png" alt=""></div>
+						<div class="row specification eminities">
+							<ul class="nav">
+								<li><span><img src="images\icons\fleet-list\1.png" alt=""></span>Auto trasition</li>
+								<li><span><img src="images\icons\fleet-list\2.png" alt=""></span>06 list/100 kms</li>
+								<li><span><img src="images\icons\fleet-list\3.png" alt=""></span>02 seats</li>
+								<li><span><img src="images\icons\fleet-list\4.png" alt=""></span>Make Year 2016</li>
+								<li><span><img src="images\icons\fleet-list\5.png" alt=""></span>04 air bags</li>
+								<li><span><img src="images\icons\fleet-list\6.png" alt=""></span>100 lits capity</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-7 col-sm-12 col-xs-12">
+						<div class="media-body this-body">
+							<div class="media this-header">
+								<div class="media-body">
+									<h2 class="rent">SUV</h2>						
+								</div>
+							</div>
+							<div class="summary row m0">
+								Rental car companies are notorious for finding additional surcharges, sometimes not added until the point of sale. These can include location or airport taxe...
+							</div>
+							<form action="login.php"> 
+								<div class="row specification car-type">
+								    <label for="Indica Vista">Indica Vista</label>
+									<label for="Suzuki Swift">Suzuki Swift</label>
+									<label for="Hyundai Eon">Hyundai Eon</label>
+									<label for="Toyota Liva">Toyota Liva</label>
+									<label for="Dustan Go">Dustan Go</label>
+								</div>
+								<div class="row specification">
+									<input type="submit" value="Book a cab" class="btn btn-primary">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>				
+			</div>
+
+		</div>
+	</section>
+  	
+   
+<?php include 'footer.php';?>

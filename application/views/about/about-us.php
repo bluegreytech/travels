bc1d630ee310d6c13b53e5d7154bbbf757ab9f77
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-12-24 09:52:50 -0500

    add

--- application/views/about/about-us.php
+++ application/views/about/about-us.php
@@ -15,12 +15,12 @@
   	<section class="wrapper-about">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-5 col-sm-12 col-xs-12">
+				<div class="col-md-6 col-sm-12 col-xs-12">
 					<div class="media-left">
-						<span><img src="<?php echo base_url();?>assets/images/services/7.jpg" alt=""></span>
+						<span><img src="<?php echo base_url();?>assets/images/about/about-us.png" alt="" width="100%"></span>
 					</div>
 				</div>
-				<div class="col-md-7 col-sm-12 introduction">  			
+				<div class="col-md-6 col-sm-12 introduction">  			
 					<div class="row section-title m0">
 						<h6 class="this-top">Introduction</h6>
 						<h2 class="h1 this-main">Who We Are</h2>
@@ -35,7 +35,7 @@
 		</div>
   	</section>  	
 
-  	<section class="row missin-benefits">
+  	<section class="row missin-benefits nopadding">
   		<div class="container">
   			<div class="row mis-be ipad-width">
   				<div class="col-md-6 col-sm-12 col-xs-12 mission-it">
@@ -46,17 +46,25 @@
   					<h3 class="this-title">Our Vision</h3>
   					<p class="this-para">We strive each day to expand our company and make it accessible to a larger population. We target to capture the entire Gujarat market and also aspire to spread it over domestic borders.</p>
   				</div>
-  				<div class="col-md-12 col-sm-12 col-xs-12 benefit-it">
-  					<h3 class="this-title">What do we offer?</h3>
-  					<p class="this-para">With the increased amount of rush going around, often people fall prey to weak transport facility which leads to delayed meetings and hypertension. However,even  when  the  people  are  getting  a  cab  facility,  they  are charged with unacceptably high rates.</p>
-  					<p class="this-para">Yashdeep Travels is a place where you find easily accessible car rides at the  most  affordable  price  equipped  with  modernized  technology  making the entire experience seamless for its clients.</p>
-  					<p class="this-para">Our  efficient  team  has  toiled  around  the  clock  to  makeit  a  customer-oriented  service,  where  we  encourage  feedback  from  our  clients.  Our client’s safety is one of the core values we maintain.Yashdeep  Travel’s  mission  entirely  works  on  the  convenience  of  the clients.</p>
-				</div>
   			</div>
   		</div>
   	</section>
 
+    <section class="row missin-benefits">
+      <div class="container">
+        <div class="row mis-be ipad-width">
+          <div class="col-md-12 col-sm-12 col-xs-12 mission-it">
+            <h3 class="this-title">What do we offer?</h3>
+            <p class="this-para">With the increased amount of rush going around, often people fall prey to weak transport facility which leads to delayed meetings and hypertension. However,even  when  the  people  are  getting  a  cab  facility,  they  are charged with unacceptably high rates.</p>
+            <p class="this-para">Yashdeep Travels is a place where you find easily accessible car rides at the  most  affordable  price  equipped  with  modernized  technology  making the entire experience seamless for its clients.</p>
+            <p class="this-para">Our  efficient  team  has  toiled  around  the  clock  to  makeit  a  customer-oriented  service,  where  we  encourage  feedback  from  our  clients.  Our client’s safety is one of the core values we maintain.Yashdeep  Travel’s  mission  entirely  works  on  the  convenience  of  the clients.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
   
+
 	  <?php 
 	 $this->load->view('common/footer');
 	 ?>

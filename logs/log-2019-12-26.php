3474b03a37f9036a91f38c84356d1a1f2cfeadf5
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-12-26 08:46:54 -0500

    add

---
+++ logs/log-2019-12-26.php
@@ -0,0 +1,111 @@
+ERROR - 2019-12-26 14:02:04 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:02:04 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:02:47 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:02:47 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:02:47 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:03:02 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:03:03 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:03:24 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:03:24 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:03:49 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:03:49 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:03:49 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:04:02 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:04:02 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:04:02 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:04:50 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:04:50 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:04:50 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:06:56 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:06:56 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:06:56 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:06:56 --> 404 Page Not Found: Assets/css
+ERROR - 2019-12-26 14:06:57 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:06:57 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:07:09 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:07:09 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:07:17 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:07:17 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:07:25 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:07:25 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:07:25 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:08:14 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:08:14 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:08:15 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:08:15 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:08:15 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:08:48 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:08:48 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:08:48 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:08:49 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:08:49 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:09:18 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:09:18 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:09:18 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:09:19 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:09:19 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:10:35 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:10:35 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:10:35 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:10:43 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:10:43 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:10:43 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:10:47 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:10:48 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:10:48 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:11:15 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:11:15 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:13:28 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:13:28 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:13:30 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:13:30 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:13:47 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:13:47 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:13:47 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:14:18 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:14:18 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:14:18 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:15:45 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:15:45 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:15:53 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:15:53 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:15:53 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:16:13 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:16:13 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:16:13 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:17:22 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:17:23 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:27:55 --> Severity: Notice --> Undefined property: stdClass::$SecondTitle C:\xampp\htdocs\travels\application\views\about\about-us.php 61
+ERROR - 2019-12-26 14:27:55 --> Severity: Notice --> Undefined property: stdClass::$SecondDescription C:\xampp\htdocs\travels\application\views\about\about-us.php 66
+ERROR - 2019-12-26 14:27:55 --> Severity: Notice --> Undefined property: stdClass::$ThirdTitle C:\xampp\htdocs\travels\application\views\about\about-us.php 73
+ERROR - 2019-12-26 14:27:55 --> Severity: Notice --> Undefined property: stdClass::$ThirdDescription C:\xampp\htdocs\travels\application\views\about\about-us.php 78
+ERROR - 2019-12-26 14:27:55 --> Severity: Notice --> Undefined property: stdClass::$FourthTitle C:\xampp\htdocs\travels\application\views\about\about-us.php 92
+ERROR - 2019-12-26 14:27:55 --> Severity: Notice --> Undefined property: stdClass::$FourthDescription C:\xampp\htdocs\travels\application\views\about\about-us.php 97
+ERROR - 2019-12-26 14:27:55 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:28:01 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:28:01 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:35:31 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:35:32 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:35:59 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:36:00 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:38:58 --> 404 Page Not Found: Assets/js
+ERROR - 2019-12-26 14:38:58 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:38:58 --> 404 Page Not Found: Assets/vendors
+ERROR - 2019-12-26 14:44:09 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:44:09 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:45:57 --> Query error: Table 'travelsdb.tblcarbrand' doesn't exist - Invalid query: SELECT *
+FROM `tblcarbrand`
+WHERE `IsDelete` = '0'
+ORDER BY `CarBrandId` DESC
+ERROR - 2019-12-26 14:45:59 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:46:01 --> Query error: Table 'travelsdb.tblcarbrand' doesn't exist - Invalid query: SELECT *
+FROM `tblcarbrand`
+WHERE `IsDelete` = '0'
+ORDER BY `CarBrandId` DESC
+ERROR - 2019-12-26 14:46:04 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:46:15 --> Severity: Notice --> Undefined property: stdClass::$OtherContactNumber C:\xampp\htdocs\travels\application\views\common\footer.php 42
+ERROR - 2019-12-26 14:46:15 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-26 14:46:16 --> Query error: Table 'travelsdb.tblcarbrand' doesn't exist - Invalid query: SELECT *
+FROM `tblcarbrand`
+WHERE `IsDelete` = '0'
+ORDER BY `CarBrandId` DESC

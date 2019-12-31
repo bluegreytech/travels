daf5f21e6a2e3d763183483bd679a0c17ecc6070
veeru225 <52230630+veeru225@users.noreply.github.com>
2019-12-31 03:45:49 -0500

    add

---
+++ logs/log-2019-12-30.php
@@ -0,0 +1,6 @@
+ERROR - 2019-12-30 11:15:27 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-30 11:19:28 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-30 11:25:19 --> Query error: Table 'travelsdb.tbluser' doesn't exist - Invalid query: INSERT INTO `tbluser` (`ContactNumber`, `OTPNumber`, `CreatedOn`) VALUES ('9976879878', '16798', '2019-12-30')
+ERROR - 2019-12-30 11:37:40 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-30 11:41:16 --> 404 Page Not Found: Assets/images
+ERROR - 2019-12-30 12:07:04 --> 404 Page Not Found: Assets/images

9645ab204a9b1963eb8d6ec97e35c5448ca67cb9
bluegreytech <52150104+bluegreytech@users.noreply.github.com>
2020-01-23 06:22:17 -0500

    add

---
+++ admin/logs/log-2020-01-23.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-23 12:18:19 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:19 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:41 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:41 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:43 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:43 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:44 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:44 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:46 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:46 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:48 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:48 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:50 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:50 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:53 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:53 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:57 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:57 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:59 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:18:59 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:02 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:02 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:07 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:07 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:11 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:11 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:26 --> Severity: Notice --> Undefined variable: CarBrandId C:\xampp\htdocs\travels\admin\application\views\fare\carfareadd.php 44
+ERROR - 2020-01-23 12:19:30 --> Severity: Notice --> Undefined variable: CarBrandId C:\xampp\htdocs\travels\admin\application\views\car\caradd.php 43
+ERROR - 2020-01-23 12:19:33 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:33 --> 404 Page Not Found: Default/images
+ERROR - 2020-01-23 12:19:45 --> Severity: Notice --> Undefined variable: CarBrandId C:\xampp\htdocs\travels\admin\application\views\fare\carfareadd.php 44
--- logs/log-2020-01-23.php
+++ logs/log-2020-01-23.php
@@ -172,3 +172,8 @@ ERROR - 2020-01-23 11:45:34 --> Severity: Notice --> Undefined index: StartCity 
 ERROR - 2020-01-23 11:45:34 --> Severity: Notice --> Undefined index: EndCity C:\xampp\htdocs\travels\application\views\home\index.php 121
 ERROR - 2020-01-23 11:45:34 --> Severity: Notice --> Undefined index: StartCity C:\xampp\htdocs\travels\application\views\home\index.php 184
 ERROR - 2020-01-23 11:45:34 --> Severity: Notice --> Undefined index: EndCity C:\xampp\htdocs\travels\application\views\home\index.php 220
+ERROR - 2020-01-23 12:18:05 --> Severity: Notice --> Undefined index: StartCity C:\xampp\htdocs\travels\application\views\home\index.php 84
+ERROR - 2020-01-23 12:18:06 --> Severity: Notice --> Undefined index: EndCity C:\xampp\htdocs\travels\application\views\home\index.php 121
+ERROR - 2020-01-23 12:18:06 --> Severity: Notice --> Undefined index: StartCity C:\xampp\htdocs\travels\application\views\home\index.php 184
+ERROR - 2020-01-23 12:18:06 --> Severity: Notice --> Undefined index: EndCity C:\xampp\htdocs\travels\application\views\home\index.php 220
+ERROR - 2020-01-23 12:18:07 --> 404 Page Not Found: Assets/images
